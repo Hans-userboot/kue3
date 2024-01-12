2baaff5ef0bc3b1c5a21f1bc87379f4fe96d8b29
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2024-01-12 12:55:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://easrkkqd:UoIT4H-p6waNT1IK6C9-yBAW0FeDrdUs@ella.db.elephantsql.com/easrkkqd")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001802984141"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002015362022"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001819953269"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001726368069"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
